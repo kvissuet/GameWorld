8a1f120f31a4347c180338514e2bb2d96c9ca32a
Kevin Vissuet <kvissuet@Kevins-MacBook-Pro.local>
2018-08-25 10:21:34 -0500

    Deleted Git History, 4th Commit

---
+++ routes/api/posts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/posts/test
+// @desc    Test posts route
+// @acess   Public
+router.get('/test',(req, res) => res.json({msg:"Posts Works"})
+);
+
+module.exports = router;
---
+++ validation/login.js
@@ -0,0 +1,34 @@
+const Validator = require('validator');
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+    let errors = {};
+
+    data.name= !isEmpty(data.name) ? data.name: "";
+    data.email= !isEmpty(data.email) ? data.email: "";
+    data.password= !isEmpty(data.password) ? data.password: "";
+    data.password2= !isEmpty(data.password2) ? data.password2: "";
+
+
+
+
+
+    if(!Validator.isEmail(data.email)){
+        errors.email = "Email is invalid";
+    }
+
+    if(Validator.isEmpty(data.email)){
+        errors.email = "Email field is required";
+    }
+
+    if(Validator.isEmpty(data.password)){
+        errors.password = "Password field is required";
+    }
+
+
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+};
